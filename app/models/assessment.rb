3a003656842c09cd6be37273ed14301a9590b9ff
Micah Gideon Modell <micah.modell@gmail.com>
2017-01-17 18:35:20 +0900

    30 tests are now passing! Now I need to start bringing in controllers...

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,5 +1,6 @@
 class Assessment < ActiveRecord::Base
   belongs_to :project
+  validates :end_date, :start_date, :assessment_id, :presence => true
 
   def self.build_new_assessment( project )
     init_date = Date.today.beginning_of_day
@@ -23,11 +24,26 @@ class Assessment < ActiveRecord::Base
     existing_assessment_count = project.assessments.where(
       "start_date = ? AND end_date = ?",
       assessment.start_date.to_date,
-      assessment.end_date.to_date ).count
-      if( existing_assessment_count == 0 )
-         project.assessments << assessment
-         project.save
+    assessment.end_date.to_date ).count
+    if( existing_assessment_count == 0 )
+      assessment.project = project
+      #project.assessments << assessment
+      project.save
+    end
+  end
+
+  def self.set_up_assessments
+    #TODO: Add timezone support here
+    init_date = Date.today.beginning_of_day
+    init_day = init_date.wday
+    Project.where(
+      "active = true AND start_date <= ? AND end_date >= ?",
+    init_date, init_date.end_of_day ).each do |project|
+
+      if( project.is_available? )
+        self.build_new_assessment project
       end
+    end
   end
 
 end
