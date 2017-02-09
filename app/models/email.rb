1cef6c01a2f0612169a7e1ea065308d0572c32e7
Micah Gideon Modell <micah.modell@gmail.com>
2017-02-09 17:20:02 +0900

    Converted to mlti-email for devise

---
+++ app/models/email.rb
@@ -0,0 +1,8 @@
+class Email < ActiveRecord::Base
+  belongs_to :user, inverse_of: :emails
+
+        def send_devise_notification(notification, *args)
+          devise_mailer.send(notification, self, *args).deliver_later
+        end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable,
+  has_many :emails, inverse_of: :user
+
+  devise :multi_email_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, 
+         :multi_email_validatable, :multi_email_confirmable,
+         :lockable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_and_belongs_to_many :groups
