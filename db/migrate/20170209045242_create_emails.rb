98e9cc823e3135eaa75fa603cefa2bdf2066e64b
Micah Gideon Modell <micah.modell@gmail.com>
2017-02-09 17:24:00 +0900

    Converted to mlti-email for devise

---
+++ db/migrate/20170209045242_create_emails.rb
@@ -0,0 +1,15 @@
+class CreateEmails < ActiveRecord::Migration
+  def change
+    create_table :emails do |t|
+      t.references :user, index: true, foreign_key: true
+      t.string :email
+      t.boolean :primary, default: false
+      t.string :confirmation_token
+      t.string :unconfirmed_email
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208064148) do
+ActiveRecord::Schema.define(version: 20170209064126) do
 
   create_table "age_ranges", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -103,6 +103,20 @@ ActiveRecord::Schema.define(version: 20170208064148) do
   add_index "diagnoses", ["behavior_id"], name: "index_diagnoses_on_behavior_id", using: :btree
   add_index "diagnoses", ["reaction_id"], name: "index_diagnoses_on_reaction_id", using: :btree
   add_index "diagnoses", ["week_id"], name: "index_diagnoses_on_week_id", using: :btree
+
+  create_table "emails", force: :cascade do |t|
+    t.integer  "user_id",              limit: 4
+    t.string   "email",                limit: 255
+    t.boolean  "primary",                          default: false
+    t.string   "confirmation_token",   limit: 255
+    t.string   "unconfirmed_email",    limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+  end
+
+  add_index "emails", ["user_id"], name: "index_emails_on_user_id", using: :btree
 
   create_table "experiences", force: :cascade do |t|
     t.integer  "course_id",  limit: 4
@@ -264,7 +278,6 @@ ActiveRecord::Schema.define(version: 20170208064148) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
@@ -296,7 +309,6 @@ ActiveRecord::Schema.define(version: 20170208064148) do
 
   add_index "users", ["age_range_id"], name: "index_users_on_age_range_id", using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["gender_id"], name: "index_users_on_gender_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
@@ -332,6 +344,7 @@ ActiveRecord::Schema.define(version: 20170208064148) do
   add_foreign_key "diagnoses", "behaviors"
   add_foreign_key "diagnoses", "reactions"
   add_foreign_key "diagnoses", "weeks"
+  add_foreign_key "emails", "users"
   add_foreign_key "experiences", "courses"
   add_foreign_key "groups", "projects"
   add_foreign_key "installments", "assessments"
