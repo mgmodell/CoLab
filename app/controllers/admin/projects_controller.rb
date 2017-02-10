eb3bc8d8ae7866a8406ce041e9a64521fa661faa
Micah Gideon Modell <micah.modell@gmail.com>
2017-02-10 17:55:04 +0900

    Beginning the admin build

---
+++ app/controllers/admin/projects_controller.rb
@@ -0,0 +1,22 @@
+class Admin::ProjectsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ app/helpers/admin/experiences_helper.rb
@@ -0,0 +1,2 @@
+module Admin::ExperiencesHelper
+end
