5ff07d65d526034b61212cf54d7f019e22289109
Micah Gideon Modell <micah.modell@gmail.com>
2017-02-16 17:49:03 +0900

    Cleaning up the UI

--- app/models/week.rb
+++ app/models/week.rb
@@ -3,4 +3,8 @@ class Week < ActiveRecord::Base
   has_one :behavior, through: :narrative
 
   has_many :diagnoses, inverse_of: :week
+
+  def percent_complete
+    percent_complete = (100 * ( self.week_num.to_f / narrative.weeks.count ) ).to_i
+  end
 end
