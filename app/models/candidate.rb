0e23ff64344aba6f1b47912f458f698693de3747
Micah Gideon Modell <micah.modell@gmail.com>
2017-03-20 02:17:40 +0900

    Rebuilt the diagrams

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,5 @@
 class Candidate < ActiveRecord::Base
   belongs_to :candidate_list, inverse_of: :candidates
-  belongs_to :candidate_feedback, inversea_of: :candidates
+  belongs_to :candidate_feedback, inverse_of: :candidates
   belongs_to :concept, inverse_of: :candidates
 end
