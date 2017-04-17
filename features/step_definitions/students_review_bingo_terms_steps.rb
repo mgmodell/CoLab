60956b14f96f57f51798b9d63cf946ac85b3c4c4
Micah Gideon Modell <micah.modell@gmail.com>
2017-04-17 23:50:32 +0900

    Fixed a few style issues.

--- features/step_definitions/students_review_bingo_terms_steps.rb
+++ features/step_definitions/students_review_bingo_terms_steps.rb
@@ -1,16 +1,15 @@
-Given /^the user is any student in the course$/  do
+Given /^the user is any student in the course$/ do
   @user = @course.rosters.enrolled.sample.user
 end
 
-Then /^the user clicks the link to the concept list$/  do
+Then /^the user clicks the link to the concept list$/ do
   click_link_or_button 'Terms for: ' + @bingo.name
 end
 
-Then /^the concept list should match the list$/  do
+Then /^the concept list should match the list$/ do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then /^the user should see (\d+) concepts$/  do |concept_count|
+Then /^the user should see (\d+) concepts$/ do |_concept_count|
   pending # Write code here that turns the phrase above into concrete actions
 end
-
