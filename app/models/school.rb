d6060d57a932454ed1bf48bc3aefe9848da7dd72
Micah Gideon Modell <micah.modell@gmail.com>
2017-05-04 15:51:16 +0900

    Added some relationships to Schools.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 class School < ActiveRecord::Base
   has_many :courses, inverse_of: :school, dependent: :destroy
+  has_many :bingo_games, through: :courses
+  has_many :projects, through: :courses
+  has_many :experiences, through: :courses
+  has_many :rosters, through: :courses
+
+  def instructors
+    rosters.joins( :role ).where( roles: { name: "Instructor" } ).collect{ |r| r.user }.uniq
+  end
+
+  def enrolled
+    rosters.joins( :role ).where( roles: { name: "Enrolled Student" } ).collect{ |r| r.user }.uniq
+  end
 end
