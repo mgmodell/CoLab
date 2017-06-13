a22f76601bbe83f4c088b5f43d0a0c519cb8ad39
Micah Gideon Modell <micah.modell@gmail.com>
2017-06-13 12:37:39 +0900

    Adding some UI changes around confirmed vs. primary.

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,7 +2,16 @@
 class Email < ActiveRecord::Base
   belongs_to :user, inverse_of: :emails
 
+  validate :check_confirmed
+
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  protected
+  def check_confirmed
+    if primary && !confirmed?
+      errors.add(:primary, "An email must be confirmed before you can make it primary." )
+    end
   end
 end
