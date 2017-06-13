aca54bfc0078e0233b63d2a7b75ccc7c25360c56
Micah Gideon Modell <micah.modell@gmail.com>
2017-06-13 11:52:55 +0900

    Configuration tests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.sendmail_settings = { 
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
