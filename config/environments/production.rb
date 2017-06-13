545b8ebc5deb9c0ec347f44f8bb29eef54e8167c
Micah Gideon Modell <micah.modell@gmail.com>
2017-06-13 17:43:40 +0900

    Perhaps a dumb mistake was just fixed?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'www.CoLab.online',
                                                domain: 'CoLab.online' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.sendmail_settings = { 
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'colab-test.herokuapp.com',
+    :domain => 'colab-testing.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
