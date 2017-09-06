624ca4b1310905011c32b0904d9bff523f6e5e3d
Micah Gideon Modell <micah.modell@stonybrook.edu>
2017-09-06 08:38:23 +0000

    Updated content checks in tests. edited online with Bitbucket

--- features/step_definitions/profile_steps.rb
+++ features/step_definitions/profile_steps.rb
@@ -1,19 +1,21 @@
 Then /^user opens their profile$/ do
   click_link_or_button('Profile')
+  page.should have_content( 'Edit your profile' )
+  page.should have_content( 'Tell us about yourself, ' + @user.first_name )
   @user.emails.each do |email|
     page.should have_content( email.email )
   end
 end
 
 Then /^the user sees the experience in the history$/  do
-  page.should have_content( @experience.get_name false )
+  page.should have_content( @experience.get_name( false ) )
 end
 
 Then /^user sees the Bingo! in the history$/  do
-  page.should have_content( @bingo.get_name false )
+  page.should have_content( @bingo.get_name( false ) )
 end
 
 Then /^user sees the assessed project in the history$/  do
-  page.should have_content( @project.get_name false )
+  page.should have_content( @project.get_name( false ) )
 end
 
