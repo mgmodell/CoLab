a6dfda5ba2a1a59a9047e635badac959d927ff2f
Micah Gideon Modell <micah.modell@gmail.com>
2017-10-22 15:23:27 +0900

    Fixing the new indices

--- db/migrate/20171022013628_add_index_for_bingo_optimization.rb
+++ db/migrate/20171022013628_add_index_for_bingo_optimization.rb
@@ -3,7 +3,5 @@ class AddIndexForBingoOptimization < ActiveRecord::Migration
     add_index :concepts, :name, name: 'concept_fulltext', type: :fulltext
     add_index :candidates, :term, length: 2
     add_index :candidates, :definition, length: 2
-    add_index :users, :bingo_boards_id
-    add_index :users, :primary_language_id
   end
 end
