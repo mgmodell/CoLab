d08ad0b1d77b760f27687488e1ca5e1991777fd5
Micah Gideon Modell <micah.modell@gmail.com>
2017-11-08 12:47:31 +0900

    Making yesterday's fix global. Ooops.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -6,7 +6,9 @@ class Candidate < ActiveRecord::Base
   belongs_to :user, inverse_of: :candidates
 
   default_scope { order(:filtered_consistent) }
-  scope :completed, -> { where("term != '' AND definition != ''") }
+  scope :completed, -> { joins(:candidate_list).
+                        where("term != '' AND definition != ''").
+                        where('candidate_lists.is_group = 0 OR candidate_lists.group_id IS NOT NULL' )}
   scope :reviewed, -> { where('candidate_feedback_id > 0 ') }
   before_save :clean_data
   validate :concept_assigned
