807ae279f1c4d6422368d884562639ae92fe9a20
Micah Gideon Modell <micah.modell@gmail.com>
2018-02-02 12:23:36 +0900

    Pushing for creater consistency

--- app/helpers/personalization_helper.rb
+++ app/helpers/personalization_helper.rb
@@ -8,16 +8,19 @@ module PersonalizationHelper
     @current_research || @current_user.anonymize?
   end
 
-  def in_tz( date:, tz_str: @current_user.timezone )
+  def in_tz( date: )
     pd = nil
+    puts "me"
+    puts "\t\tunProc: #{date}"
     unless date.nil?
-      tz = ActiveSupport::TimeZone.new( tz_str )
+      tz = ActiveSupport::TimeZone.new( @current_user.timezone )
       pd = tz.local( date.year, date.month, date.day, date.hour, date.min )
     end
+    puts "\t\tProc: #{pd}"
     pd
   end
 
-  def convert_to_tz( date:, tz_str: @current_user.timezone )
+  def to_tz( date:, tz_str: )
     pd = nil
     unless date.nil?
       tz = ActiveSupport::TimeZone.new( tz_str )
