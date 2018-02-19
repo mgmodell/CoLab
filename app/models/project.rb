46a17354615911075100014d6e06f37f16ed6781
Micah Gideon Modell <micah.modell@gmail.com>
2018-02-19 22:19:19 +0900

    upgrade to 5.2 successful

--- app/models/project.rb
+++ app/models/project.rb
@@ -167,12 +167,12 @@ class Project < ApplicationRecord
   end
 
   def activation_status
-    if active_was && active &&
+    if active_before_last_save && active &&
        (start_dow_changed? || end_dow_changed? ||
         start_date_changed? || end_date_changed? ||
         factor_pack_id_changed? || style_id_changed?)
       self.active = false
-    elsif !active_was && active
+    elsif !active_before_last_save && active
 
       get_user_appearance_counts.each do |user_id, count|
         # Check the users
@@ -213,14 +213,14 @@ class Project < ApplicationRecord
     # TZ corrections
     if start_date.nil? || start_date.change(hour: 0) == course.start_date.change(hour: 0)
       self.start_date = course.start_date
-    elsif start_date_changed?
+    elsif start_date_change_to_be_saved
       proc_date = course_tz.local(start_date.year, start_date.month, start_date.day)
       self.start_date = proc_date.beginning_of_day
     end
 
     if end_date.nil? || end_date.change(hour: 0) == course.end_date.change(hour: 0)
       self.end_date = course.end_date
-    elsif end_date_changed?
+    elsif end_date_change_to_be_saved
       proc_date = course_tz.local(end_date.year, end_date.month, end_date.day)
       self.end_date = proc_date.end_of_day.change(sec: 0)
     end
