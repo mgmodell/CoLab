9e0ba74f353b615a9caaa33099958e50072afd0d
Micah Gideon Modell <micah.modell@gmail.com>
2018-12-17 17:21:21 +0900

    removing sass and cleaning up js tests

--- Gemfile
+++ Gemfile
@@ -9,8 +9,7 @@ gem 'rails', '~> 5.2.2'
 gem 'puma', '~> 3.7'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.18', '< 0.5'
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
@@ -71,6 +70,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'bullet'
   gem 'byebug'
+  gem 'chromedriver-helper'
   gem 'cucumber-rails', require: false
   gem 'database_cleaner'
   gem 'htmlbeautifier'
--- features/support/env.rb
+++ features/support/env.rb
@@ -7,8 +7,23 @@
 # files.
 
 require 'cucumber/rails'
+require "selenium/webdriver"
 
-Capybara.javascript_driver = :selenium
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
 Capybara.default_driver = :rack_test
 Cucumber::Rails::Database.autorun_database_cleaner = false
 
