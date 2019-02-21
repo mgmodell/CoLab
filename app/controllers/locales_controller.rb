a192e1173a6db5c08c8f6b24c88105dbe0b36ef8
Micah Gideon Modell <micah.modell@gmail.com>
2019-02-21 14:58:33 +0900

    updating and complicating locales

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,12 +1,18 @@
 class LocalesController < ApplicationController
 
   def get_resources
-    lang = params[:lang]
     ns = params[:ns]
-    lang_was = I18n.locale
+    lang = params[:lang]
+
+    #TODO: build a more robust solution
+    if @current_user.present?
+      lang = @current_user.language.code
+    else
+      lang = 'en'
+    end
+
     I18n.locale = lang
     texts = I18n.t ns 
-    I18n.locale = lang_was
 
     render json: texts
   end
