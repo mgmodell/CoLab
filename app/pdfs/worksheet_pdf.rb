1e627a6bd77d1eff13748f555761ae42f3f92223
Micah Gideon Modell <micah.modell@gmail.com>
2019-03-04 09:59:22 +0900

    fixing a rushed fix

--- app/pdfs/worksheet_pdf.rb
+++ app/pdfs/worksheet_pdf.rb
@@ -52,8 +52,7 @@ class WorksheetPdf
     stroke_horizontal_rule
     move_down 5
     items.each do |item|
-      item[2].remove!( '(', ')' )
-      item[2].name.split(/\W+/).each do |w|
+      item[2].name.remove( '(', ')' ).split(/\W+/).each do |w|
         item[1].gsub!(/\b#{w}/, ('*' * w.length))
       end
       text "<b>#{item[0]}.</b>  #{item[1]}",
