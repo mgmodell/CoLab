fa98e25cbbe43a7205a61ab7792a270b4eefc13e
Micah Gideon Modell <micah.modell@gmail.com>
2019-04-15 12:49:26 +0900

    calendar css not yet working

--- app/javascript/components/CourseCalendar.js
+++ app/javascript/components/CourseCalendar.js
@@ -1,16 +1,47 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Paper from '@material-ui/core/Paper'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import listPlugin from '@fullcalendar/list'
 import momentPlugin from '@fullcalendar/moment'
+
+import '@fullcalendar/core/main.css'
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/list/main.css';
+
 
 class CourseCalendar extends React.Component {
   render () {
+    const header = {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth, dayGridWeek, listMonth'
+        }
+
+    function clickedMe( event ){
+      console.log( event )
+      console.log( event.title )
+
+    }
+
     return (
+    <Paper>
       <FullCalendar
         defaultView="dayGridMonth"
-        plugins={[ dayGridPlugin ]}
+        plugins={[ dayGridPlugin, listPlugin ]}
+        header={header}
+        eventClick={clickedMe}
+        themeSystem='standard'
+        events={[
+          { title: 'event 1', allDay: true,
+            start: '2019-04-22', end: '2019-4-22' },
+          { title: 'event 2', allDay: true,
+            start: '2019-04-25', end: '2019-4-30' }
+        ]}
+          
       />
+    </Paper>
     );
   }
 }
