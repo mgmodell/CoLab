7291222663be59ed941896c27a50b524572dfb61
Micah Gideon Modell <micah.modell@gmail.com>
2022-05-03 13:09:05 -0400

    more docker fixes

--- initDBs.sql
+++ initDBs.sql
@@ -1,14 +1,6 @@
-CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'test';
+CREATE USER IF NOT EXISTS 'test'@'172.%' IDENTIFIED BY 'test';
 
-GRANT ALL PRIVILEGES ON colab_dev.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_1.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_2.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_3.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_4.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_5.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_6.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_7.* TO 'test'@'localhost';
-GRANT ALL PRIVILEGES ON colab_test_8.* TO 'test'@'localhost';
+GRANT ALL PRIVILEGES ON colab_dev.* TO 'test'@'172.%';
+GRANT ALL PRIVILEGES ON `colab_test\_%`.* TO 'test'@'172.%';
 
 FLUSH PRIVILEGES;
