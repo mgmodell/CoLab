ad1ac8c1030da40d27faa88ffce89345eddc811d
Micah Gideon Modell <micah.modell@gmail.com>
2022-05-09 21:02:57 -0400

    fixing env variable

--- run_cukes.sh
+++ run_cukes.sh
@@ -7,4 +7,4 @@ asdf install
 bundle install
 yarn install
 
-rails cucumber:rerun RAILS_ENV=docker CUCUMBER_PUBLISH_TOKEN=caa67d94-0eab-4593-90c7-6032772d86ec
+rails cucumber:rerun DRIVER=docker CUCUMBER_PUBLISH_TOKEN=caa67d94-0eab-4593-90c7-6032772d86ec
