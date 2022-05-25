d89ab75fb3cee33f21b4e5668cbd27f795060392
Stephanie Montano <smon2175@my.msmc.edu>
2022-05-25 14:43:09 -0400

    minor script fixes

--- containers/initDBs.sql
+++ containers/initDBs.sql
@@ -1,6 +1,9 @@
 CREATE USER IF NOT EXISTS 'test'@'172.%' IDENTIFIED BY 'test';
+CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'test';
 
 GRANT ALL PRIVILEGES ON colab_dev.* TO 'test'@'172.%';
+GRANT ALL PRIVILEGES ON colab_dev.* TO 'test'@'localhost';
 GRANT ALL PRIVILEGES ON `colab_test\_%`.* TO 'test'@'172.%';
+GRANT ALL PRIVILEGES ON `colab_test\_%`.* TO 'test'@'localhost';
 
 FLUSH PRIVILEGES;
