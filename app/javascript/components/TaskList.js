4a625dfbf32f07416b3aa1862733cfc06339a11c
Micah Gideon Modell <micah.modell@gmail.com>
2022-07-05 22:15:59 -0400

    debugging and version bumps

--- app/javascript/components/TaskList.js
+++ app/javascript/components/TaskList.js
@@ -117,10 +117,10 @@ export default function TaskList(props) {
           var retVal = 'n/a';
           if( null !== value ){
             const dt = DateTime.fromISO(value);
-            console.log( dt.zoneName );
+            console.log( 'pre', dt.zoneName );
             dt.setZone( Settings.defaultZone );
-            console.log( Settings.defaultZone );
-            console.log( dt.zoneName );
+            console.log( 'default', Settings.defaultZone );
+            console.log( 'post', dt.zoneName );
             retVal = ( <span>{dt.toLocaleString(DateTime.DATETIME_MED)} ({dt.zoneName} )</span> );
           }
           return retVal;
