6ff7d2b3571e05cd1a223a7ac3294498a812fc94
Micah Gideon Modell <micah.modell@gmail.com>
2022-12-18 18:30:35 +0000

    minor cleanup on run script

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,9 @@
 
 # MOVE THIS PULL AND RISK STAGNATION
 echo "Setting the current working directory"
-. $HOME/.asdf/asdf.sh
-cd $HOME/src/app
-git pull
-asdf reshim
 
 print_help ( ) {
-  echo "RunCukes: Script to launch automated tests"
+  echo "run_tests.sh: Script to launch containerized automated tests"
   echo "Valid options:"
   echo " -s             Show the failures from previous run only"
   echo "                (Do not run any tests.)"
@@ -16,6 +12,7 @@ print_help ( ) {
   echo " -c             Initialise the database and terminate"
   echo " -l             Clear out the log files"
   echo " -n             Wipe previous runs"
+  echo " -o		Show output (if running)"
   echo "       Run-specific options:"
   echo " -d [driver]    Set the browser driver for this run"
   echo " -f [features]  Specify specific features to run"
@@ -29,11 +26,21 @@ print_help ( ) {
 
 }
 
-TERMINAL=false
-while getopts "h" opt; do
+if [ "$#" -lt 1 ]; then
+  echo "Please specify options"
+  print_help
+fi
+
+RUN_TERM=false
+while getopts "sob:clndfrteh" opt; do
   case $opt in
-    t) #Invalid option
-      TERMINAL=true
+    t) # Open up a terminal
+      RUN_TERM=true
+      ;;
+    o)
+      OUTPUT_HASH=`docker ps | grep colab_tester | awk '{print $1;}'`
+      docker logs -f $OUTPUT_HASH
+      exit 0;
       ;;
     h|\?) #Invalid option
       print_help
@@ -42,8 +49,11 @@ while getopts "h" opt; do
   esac
 done
 
-if [ "$TERMINAL" = true] then
-  docker-compose run --rm app $@
+pushd containers/test_env/
+if [ "$RUN_TERM" = true ]; then
+  docker-compose run --rm --entrypoint='' app /bin/bash
+  # docker-compose run --rm app $@
 else
   docker-compose run --rm -d app $@
 fi
+popd
