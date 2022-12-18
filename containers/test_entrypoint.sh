aadef68a8321219bc348ed1b38f73c8c02f6d2a0
Micah Gideon Modell <micah.modell@gmail.com>
2022-12-17 23:53:56 -0500

    dev server is nearly functional

--- containers/test_entrypoint.sh
+++ containers/test_entrypoint.sh
@@ -7,9 +7,11 @@ dir="$HOME/src/app/"
 
 if [[ $(find ${dir} -type f  | wc -l) -lt 1 ]]; then
   git clone git@bitbucket.org:_performance/colab.git $dir
-  cd $dir
-  git pull
 fi
+
+cd $dir
+# Make sure we're working with the latest
+git pull
 
 $HOME/src/app/run_cukes.sh "$@"
 
---
+++ dev_serv.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+print_help ( ) {
+  echo "StartDev: Script to launch containerized dev server"
+  echo "Valid options:"
+  echo " -s             Start the server (cannot be combined)"
+  echo " -x             Stop the server (cannot be combined)"
+  echo ""
+  echo " -l [sql dump]  Load DB from dump (assumes -m)"
+  echo " -d             Migrate the DB"
+  echo " -m [task]      Run a migratify task (assumes -m)"
+  echo ""
+  echo " *** NOT IMPLEMENTED YET ***"
+  echo " -r             Restart the server"
+  echo ""
+  echo " -h             Show this help and terminate"
+  
+  exit 0;
+
+}
+
+echo "Arguments: '$@'"
+
+if [ "$#" -lt 1 ]; then
+  echo "Please specify options"
+  print_help
+fi
+
+#Begin
+pushd containers/dev_env
+
+# Set up run context
+COLAB_DB=db
+
+DRIVER=docker
+SHOW_HELP=false
+MIGRATE=false
+RUN_TASK=false
+LOAD=false
+
+if lsof -Pi :31337 -sTCP:LISTEN -t >/dev/null; then
+  echo "DB Running"
+else
+  echo "DB needs to be started"
+  docker-compose up -d db
+  sleep 2
+  echo "DB started"
+fi
+
+DB_COUNT=`mysqlshow -u test -ptest --protocol=TCP --port=3306 | grep colab_dev | wc -l`
+if [ $(($DB_COUNT)) = 0 ]; then
+  echo "Creating the DB"
+  docker-compose run --rm app "rails db:create COLAB_DB=db COLAB_DB_PORT=3306"
+  echo "Created the DB"
+fi
+
+while getopts "tsxm:l:h" opt; do
+  case $opt in
+    t)
+      docker-compose run --rm app /bin/bash
+      popd
+      exit
+      ;;
+    s)
+      docker-compose run --rm --service-ports app "foreman start -f Procfile.dev"
+      popd
+      exit
+      ;;
+    x)
+      docker-compose down --remove-orphans
+      popd
+      exit
+      ;;
+    l)
+      MIGRATE=true
+      LOAD=true
+      LOAD_FILE=$OPTARG
+      ;;
+    m)
+      RUN_TASK=true
+      RUN_TASK_NAME=$OPTARG
+      MIGRATE=true
+      ;;
+    d)
+      MIGRATE=true
+      ;;
+    h|\?) #Invalid option
+      SHOW_HELP=true
+      ;;
+  esac
+done
+
+# Handle Command Help Request
+if [ "$SHOW_HELP" = true ]; then
+  print_help
+fi
+
+# Load a sql file
+if [ "$LOAD" = true ]; then
+  echo "Loading"
+  mysql colab_dev -u test -ptest --protocol=TCP --port=3306 < ../../$LOAD_FILE
+  echo "Loaded"
+fi
+
+# Migrate the DB
+if [ "$MIGRATE" = true ]; then
+  docker-compose run --rm app "rails db:migrate COLAB_DB=db COLAB_DB_PORT=3306"
+fi
+
+# Run a migratify task
+if [ "$MIGRATE" = true ]; then
+  docker-compose run --rm app "rails migratify:$RUN_TASK_NAME COLAB_DB=db COLAB_DB_PORT=3306"
+fi
+
+popd
+
