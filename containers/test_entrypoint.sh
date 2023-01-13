7b56df6a81939acef29164ff4bb9f509f7e6a26f
Micah Gideon Modell <micah.modell@gmail.com>
2023-01-13 09:46:48 -0500

    let's see if this works

--- containers/test_entrypoint.sh
+++ containers/test_entrypoint.sh
@@ -7,8 +7,19 @@ if [[ $(find ${dir} -type f  | wc -l) -lt 1 ]]; then
 fi
 
 cd $dir
+
 # Make sure we're working with the latest
-git pull --all
+if grep -q bitbucket .git/config; then
+  echo "Migrate from bitbucket"
+  git remote rm origin
+  git remote add origin https://github.com/mgmodell/CoLab.git
+  git pull --all
+  git push --mirror
+
+else
+  git pull --all
+
+fi
 
 $HOME/src/app/containers/test_env/run_cukes.sh "$@"
 
