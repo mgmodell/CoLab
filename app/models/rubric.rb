6316f6e8ff9da9ae2a752f7f180701bd60cb67a4
Micah Gideon Modell <micah.modell@gmail.com>
2023-02-05 21:54:27 -0500

    validation on rubrics

--- app/models/rubric.rb
+++ app/models/rubric.rb
@@ -2,4 +2,7 @@ class Rubric < ApplicationRecord
     belongs_to :parent, class_name: 'Rubric'
     has_many :versions, class_name: 'Rubric', foreign_key: 'parent_id'
 
+    # Validations
+    validates :passing, numericality: { in: 0..100 }
+
 end
