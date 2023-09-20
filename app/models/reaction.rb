90f6cbccaab32ac9f72f44a12bfcbe3c2d811ac5
Micah Gideon Modell <micah.modell@gmail.com>
2023-09-20 11:31:39 -0400

    filter out empty narratives

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -22,7 +22,7 @@ class Reaction < ApplicationRecord
       found_narrative = NilClass
       # If we've already been assigned at least one reaction
       if user.reactions.count.positive? && user.reactions.count < Narrative.all.count
-        if user.reactions.count < Scenario.all.count
+        if user.reactions.where.not(narrative_id: nil).count < Scenario.all.count
           # If they haven't yet been assigned all possible scenarios
           assigned_scenarios = user.reactions.joins(:narrative).group(:scenario_id).count
           possible_narratives = if assigned_scenarios.empty?
@@ -34,7 +34,7 @@ class Reaction < ApplicationRecord
 
         else
           # If they've been assigned all scenarios, but not all narratives
-          available_scenarios = user.reactions.group(:narrative_id).count
+          available_scenarios = user.reactions.group(:narrative_id).where.not(narrative_id: nil).count
           possible_narratives = Narrative.where('id NOT IN (?)', available_scenarios.keys)
 
         end
