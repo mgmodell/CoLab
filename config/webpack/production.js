130a14c4f5d20564c2de8b766d27ca876edbb8a0
Micah Gideon Modell <micah.modell@gmail.com>
2023-10-15 19:54:42 -0400

    webpackConfig conversion

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,9 @@
 // The source code including full typescript support is available at: 
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/production.js
 
-const webpackConfig = require('./webpackConfig');
+const { generateWebpackConfig } = require("shakapacker");
+
+const webpackConfig = generateWebpackConfig;
 
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for production only
--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,7 +1,9 @@
 // The source code including full typescript support is available at: 
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/test.js
 
-const webpackConfig = require('./webpackConfig')
+const { generateWebpackConfig } = require("shakapacker");
+
+const webpackConfig = generateWebpackConfig;
 
 const testOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for test only
