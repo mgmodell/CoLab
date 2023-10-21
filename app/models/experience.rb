7ec1604400a38d1ee39e7da844bce9cd8b606b78
Micah Gideon Modell <micah.modell@gmail.com>
2023-10-21 08:53:58 -0400

    make sure we can't overwrite the narrative

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -22,7 +22,7 @@ class Experience < ApplicationRecord
     reaction = reactions.includes(narrative: { scenario: :behavior }).find_by(user:)
 
     reaction = Reaction.create(user:, experience: self, instructed: false) if reaction.nil?
-    reaction.assign_narrative
+    reaction.assign_narrative if reaction.narrative.nil?
     reaction
   end
 
