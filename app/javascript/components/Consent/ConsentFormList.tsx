94bcde597a1f0c3ed19d65654f960e12539828c7
Micah Gideon Modell <micah.modell@gmail.com>
2023-12-06 14:15:37 -0500

    Migration from mui to prime

--- app/javascript/components/Consent/ConsentFormList.tsx
+++ app/javascript/components/Consent/ConsentFormList.tsx
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Alert from "@mui/material/Alert";
-import IconButton from "@mui/material/IconButton";
 
-import CloseIcon from "@mui/icons-material/Close";
-import CheckIcon from "@mui/icons-material/Check";
 
-import Collapse from "@mui/material/Collapse";
 import { useTypedSelector } from "../infrastructure/AppReducers";
 import { useDispatch } from "react-redux";
 import { startTask, endTask } from "../infrastructure/StatusSlice";
 import { useTranslation } from "react-i18next";
-import { DataGrid, GridCellParams, GridColDef } from "@mui/x-data-grid";
 import AdminListToolbar from "../infrastructure/AdminListToolbar";
-import { renderTextCellExpand } from "../infrastructure/GridCellExpand";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Checkbox } from "primereact/checkbox";
 
 export default function ConsentFormList(props) {
   const category = "consent_form";
@@ -30,27 +26,20 @@ export default function ConsentFormList(props) {
   const navigate = useNavigate();
 
   const user = useTypedSelector(state => state.profile.user);
+  enum OPT_COLS {
+    NAME = 'name',
+    ACTIVE = 'active',
+  }
+  const [filterText, setFilterText] = useState('');
+  const optColumns = [
+    OPT_COLS.NAME,
+    OPT_COLS.ACTIVE,
+  ];
+  const [visibleColumns, setVisibleColumns] = useState([]);
   const [messages, setMessages] = useState({});
   const [showErrors, setShowErrors] = useState(false);
 
-  const columns: GridColDef[] = [
-    {
-      headerName: t("index.name_col"),
-      field: "name",
-      width: 400,
-      renderCell: renderTextCellExpand
-    },
-    {
-      headerName: t("index.active_col"),
-      field: "active",
-      renderCell: params => {
-        const output = params.value ? <CheckIcon /> : null;
-        return output;
-      }
-    }
-  ];
-
-  const [consent_forms, setSchools] = useState([]);
+  const [consentForms, setConsentForms] = useState([]);
 
   const getSchools = () => {
     const url = endpoints.baseUrl + ".json";
@@ -61,7 +50,7 @@ export default function ConsentFormList(props) {
       .then(response => {
         const data = response.data;
         //Process the data
-        setSchools(data);
+        setConsentForms(data);
         dispatch(endTask("loading"));
       })
       .catch(error => {
@@ -82,45 +71,65 @@ export default function ConsentFormList(props) {
   };
 
   const dataTable = (
-    <DataGrid
-      isCellEditable={() => false}
-      columns={columns}
-      rows={consent_forms}
-      onCellClick={(params: GridCellParams) => {
-        navigate(String(params.row.id));
-      }}
-      slots={{
-        toolbar: AdminListToolbar
-      }}
-      slotProps={{
-        toolbar: {
-          itemType: category
+      <DataTable
+        value={consentForms.filter((course) => {
+          return filterText.length === 0 || course.name.includes(filterText);
+        })}
+        resizableColumns
+        tableStyle={{
+          minWidth: '50rem'
+        }}
+        reorderableColumns
+        paginator
+        rows={5}
+        rowsPerPageOptions={
+          [5, 10, 20, consentForms.length]
         }
-      }}
-      pageSizeOptions={[5, 10, 100]}
-    />
+        header={<AdminListToolbar
+          itemType={category}
+          filtering={{
+            filterValue: filterText,
+            setFilterFunc: setFilterText
+          }}
+          columnToggle={{
+            optColumns: optColumns,
+            visibleColumns: visibleColumns,
+            setVisibleColumnsFunc: setVisibleColumns,
+          }}
+        />}
+        sortField="start_date"
+        sortOrder={-1}
+        paginatorDropdownAppendTo={'self'}
+        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        currentPageReportTemplate="{first} to {last} of {totalRecords}"
+        dataKey="id"
+        onRowClick={(event) => {
+          navigate(String(event.data.id));
+        }}
+      >
+        <Column
+          header={t('index.name_col')}
+          field={'name'}
+          sortable
+          filter={false}
+          key={'name'}
+          />
+        <Column
+          header={t('index.active_col')}
+          field={'active'}
+          sortable
+          filter
+          key={'active'}
+          body={param =>{
+            return <Checkbox checked={param.active} disabled />
+          }}
+          />
+      </DataTable>
+
   );
 
   return (
     <React.Fragment>
-      <Collapse in={showErrors}>
-        <Alert
-          action={
-            <IconButton
-              aria-label="close"
-              color="inherit"
-              size="small"
-              onClick={() => {
-                setShowErrors(false);
-              }}
-            >
-              <CloseIcon fontSize="inherit" />
-            </IconButton>
-          }
-        >
-          {messages["main"] || null}
-        </Alert>
-      </Collapse>
       <div style={{ maxWidth: "100%" }}>{dataTable}</div>
     </React.Fragment>
   );
