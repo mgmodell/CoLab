e1eac45b0c3ae1b91496169f1c6c971b4ed28828
Micah Gideon Modell <micah.modell@gmail.com>
2023-12-21 14:45:53 -0500

    Concepts is no longer wrappedVirt

--- app/javascript/components/ConceptsTable.tsx
+++ app/javascript/components/ConceptsTable.tsx
@@ -1,27 +1,30 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogActions from "@mui/material/DialogActions";
-import DialogTitle from "@mui/material/DialogTitle";
-import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import SearchIcon from "@mui/icons-material/Search";
-import Toolbar from "@mui/material/Toolbar";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
+
 import { SortDirection } from "react-virtualized";
 import { useTypedSelector } from "./infrastructure/AppReducers";
 import { useTranslation } from "react-i18next";
 
-import WrappedVirtualizedTable from "./WrappedVirtualizedTable";
 import WorkingIndicator from "./infrastructure/WorkingIndicator";
+import StandardListToolbar from "./StandardListToolbar";
+
+import { Button } from "primereact/button";
+import { Panel } from "primereact/panel";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Dialog } from "primereact/dialog";
 
 import { useDispatch } from "react-redux";
 import { startTask, endTask } from "./infrastructure/StatusSlice";
+import { InputText } from "primereact/inputtext";
+
+enum OPT_COLS {
+  NAME = "name",
+  USE_COUNT = "use_count",
+  COURSES = "courses",
+  GAMES = "games"
+}
 
 const styles = theme => ({
   table: {
@@ -55,10 +58,18 @@ export default function ConceptsTable(props) {
   const endpointStatus = useTypedSelector(
     state => state.context.status["endpointsLoaded"]
   );
+  const { t } = useTranslation(`${category}s`);
   const dispatch = useDispatch();
 
   const [conceptsRaw, setConceptsRaw] = useState([]);
   const [concepts, setConcepts] = useState([]);
+  const [filterText, setFilterText] = useState("");
+  const optColumns = [
+    t( OPT_COLS.USE_COUNT ),
+    t( OPT_COLS.COURSES ),
+    t( OPT_COLS.GAMES )
+  ];
+  const [visibleColumns, setVisibleColumns] = useState([]);
 
   //const [search, setSearch] = useState("");
   const [sortBy, setSortBy] = useState("name");
@@ -68,43 +79,6 @@ export default function ConceptsTable(props) {
   const [dirty, setDirty] = useState(false);
   const [conceptName, setConceptName] = useState("");
   const [conceptId, setConceptId] = useState(-1);
-  const { t } = useTranslation(`${category}s`);
-
-  const columns = [
-    {
-      width: 200,
-      flexGrow: 1.0,
-      label: t("name"),
-      dataKey: "name",
-      numeric: false,
-      disableSort: false,
-      visible: true
-    },
-    {
-      width: 120,
-      label: t("use_count"),
-      dataKey: "times",
-      numeric: true,
-      disableSort: true,
-      visible: true
-    },
-    {
-      width: 120,
-      label: t("courses"),
-      dataKey: "courses",
-      numeric: true,
-      disableSort: true,
-      visible: true
-    },
-    {
-      width: 120,
-      label: t("games"),
-      dataKey: "bingos",
-      numeric: true,
-      disableSort: true,
-      visible: true
-    }
-  ];
 
   useEffect(() => {
     if (endpointStatus) {
@@ -157,8 +131,8 @@ export default function ConceptsTable(props) {
     setSortBy(event.sortBy);
   };
   const drillDown = event => {
-    setConceptId(event.rowData.id);
-    setConceptName(event.rowData.name);
+    setConceptId(event.data.id);
+    setConceptName(event.data.name);
     setEditing(true);
     setDirty(false);
   };
@@ -191,51 +165,99 @@ export default function ConceptsTable(props) {
   };
   return (
     <React.Fragment>
-      <Paper style={{ height: 450, width: "100%" }}>
-        <Toolbar>
-          <InputBase placeholder={t("search_concepts")} onChange={filter} />
-          <SearchIcon />
-          <Typography variant="h6" color="inherit">
-            Showing {concepts.length} of {conceptsRaw.length}
-          </Typography>
-        </Toolbar>
-        <WrappedVirtualizedTable
-          rowCount={concepts.length}
-          rowGetter={({ index }) => concepts[index]}
-          sort={colSort}
-          sortBy={sortBy}
-          sortDirection={sortDirection}
-          onRowClick={drillDown}
-          columns={columns}
-        />
-      </Paper>
+      <DataTable
+        value={concepts.filter(concept => {
+          return filterText.length === 0 || concept.cap_name.includes(filterText.toUpperCase());
+        })}
+        resizableColumns
+        tableStyle={{
+          minWidth: '50rem'
+        }}
+        reorderableColumns
+        paginator
+        rows={5}
+        rowsPerPageOptions={
+          [5, 10, 20, 100]
+        }
+        virtualScrollerOptions={{ itemSize: 100 }}
+        onRowClick={drillDown}
+        header={
+          <StandardListToolbar
+          itemType={'invitation'}
+          filtering={
+            {
+              filterValue: filterText,
+              setFilterFunc: setFilterText
+            }
+          }
+          columnToggle={{
+            optColumns: optColumns,
+            visibleColumns: visibleColumns,
+            setVisibleColumnsFunc: setVisibleColumns,
+          }}
+        />}
+        sortField="name"
+        paginatorDropdownAppendTo={'self'}
+        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        currentPageReportTemplate="{first} to {last} of {totalRecords}"
+        //paginatorLeft={paginatorLeft}
+        //paginatorRight={paginatorRight}
+        dataKey="id"
+        >
+          <Column
+            header={t("name")}
+            field="name"
+            sortable
+            filter
+            key="name"
+            />
+          <Column
+            header={t("use_count")}
+            field="times"
+            sortable
+            filter
+            key="times"
+            />
+          <Column
+            header={t("courses")}
+            field="courses"
+            sortable
+            filter
+            key="courses"
+            />
+          <Column
+            header={t("games")}
+            field="bingos"
+            sortable
+            filter
+            key="bingos"
+            />
+        </DataTable>
       <Dialog
-        open={editing}
-        onClose={() => setEditing(false)}
+        visible={editing}
+        onHide={() => setEditing(false)}
         aria-labelledby="edit"
-      >
-        <WorkingIndicator identifier="saving_concept" />
-        <DialogTitle> {t("edit.title")} </DialogTitle>
-        <DialogContent>
-          <DialogContentText>{t("concept_name")} </DialogContentText>
-          <TextField
-            value={conceptName}
-            onChange={event => setName(event.target.value)}
-          />
-
-          <DialogActions>
-            <Button variant="contained" onClick={() => setEditing(false)}>
+        header={t("edit.title")}
+        footer={(
+          <>
+            <Button onClick={() => setEditing(false)}>
               {t("cancel")}
             </Button>
             <Button
-              variant="contained"
               onClick={() => updateConcept(conceptId, conceptName)}
               disabled={!dirty}
             >
               {t("update_con")}
             </Button>
-          </DialogActions>
-        </DialogContent>
+          </>
+        )}
+      >
+        <WorkingIndicator identifier="saving_concept" />
+        {t('concept_name')}
+        <InputText
+          value={conceptName}
+          onChange={event => setName(event.target.value)}
+        />
       </Dialog>
     </React.Fragment>
   );
