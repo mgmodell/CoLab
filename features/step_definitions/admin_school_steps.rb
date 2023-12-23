ac02d84fd8c369ba01fb7d88dc1c426b2847940b
Micah Gideon Modell <micah.modell@gmail.com>
2023-12-22 22:22:10 -0500

    tab selectors

--- features/step_definitions/admin_school_steps.rb
+++ features/step_definitions/admin_school_steps.rb
@@ -38,7 +38,7 @@ end
 
 Then 'the user will dismiss the error {string}' do |error_message|
   page.should have_content error_message
-  find(:xpath, "//button[@id='error-close']").click
+  find_all(:xpath, "//div[@data-pc-name='toast']//button[@data-pc-section='closebutton']").each(&:click)
 end
 
 Then(/^the user waits to see "([^"]*)"$/) do |wait_msg|
