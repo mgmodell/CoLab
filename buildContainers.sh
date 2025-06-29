227942e885936cdc5444daef854ae1ca5110a5d1
Micah Gideon Modell <micah.modell@gmail.com>
2025-06-28 22:48:56 -0400

    info and versions

--- buildContainers.sh
+++ buildContainers.sh
@@ -1,50 +1,40 @@
 #!/bin/bash
 
+# This script builds the containers for the Colab project using Podman.
+
+# Function to test the result of the build command
+# Arguments:
+#   $1 - Exit status of the build command
+#   $2 - Name of the container being built
+# If the build is successful, it tags the container with 'latest' and prints a success
+# message. If it fails, it prints an error message and exits with status 1.
+test_result ( ) {
+    if [[ $1 -eq 0 ]]; then
+      podman tag $2 $2:latest
+      echo -e "\tbuild $2: succeeded"
+    else
+      echo -e "\n\t*****\n\tbuilding $2 failed\n\t*****\n"
+      exit 1
+    fi
+
+}
+
 echo -e '\t*****\n\tbuilding db'
 podman build -f ./containers/agnostic/db/Dockerfile -t colab_db .
-if [[ $? -eq 0 ]]; then
-  podman tag colab_db colab_db:latest
-  echo -e '\tbuild db: succeeded'
-else
-  echo -e '\n\t*****\n\tbuilding db failed\n\t*****\n'
-  exit 1
-fi
+test_result $? 'colab_db'
 
 echo -e '\n\t*****\n\tbuilding app tester'
 podman build -f ./containers/agnostic/tester_server/Dockerfile -t colab_tester .
-if [[ $? -eq 0 ]]; then
-  podman tag colab_tester colab_tester:latest
-  echo -e '\tbuild tester: succeeded'
-else
-  echo -e '\tbuild tester: failed\n'
-  exit 1
-fi
+test_result $? 'colab_tester'
 
 echo -e '\n\t*****\n\tbuilding app dev'
 podman build -f ./containers/agnostic/dev_server/Dockerfile -t colab_dev_server .
-if [[ $? -eq 0 ]]; then
-  podman tag colab_dev_server colab_dev_server:latest
-  echo -e '\tbuild app dev: succeeded'
-else
-  echo -e '\tbuild app dev: failed\n'
-  exit 1
-fi
+test_result $? 'colab_dev_server'
+
 echo -e '\n\t*****\n\tbuilding browser'
 podman build -f ./containers/agnostic/browser/Dockerfile -t colab_browser .
-if [[ $? -eq 0 ]]; then
-  podman tag colab_browser colab_browser:latest
-  echo -e '\tbuild browser: succeeded'
-else
-  echo -e '\tbuild browser: failed\n'
-  exit 1
-fi
+test_result $? 'colab_browser'
+
 echo -e '\n\t*****\n\tbuilding moodle'
 podman build -f ./containers/agnostic/moodle/Dockerfile -t colab_moodle .
-if [[ $? -eq 0 ]]; then
-  podman tag colab_moodle colab_moodle:latest
-  echo -e '\tbuild moodle: succeeded'
-else
-  echo -e '\tbuild moodle: failed\n'
-  exit 1
-fi
-
+test_result $? 'colab_moodle'
