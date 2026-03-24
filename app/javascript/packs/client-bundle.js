67b6125701cabffce863fccd801a6a049c0a9db2
Micah Gideon Modell <micah.modell@gmail.com>
2026-03-24 17:24:44 -0400

    restructuring and de-lazy-ifying [AppInit]

--- app/javascript/packs/client-bundle.js
+++ app/javascript/packs/client-bundle.js
@@ -1,8 +1,6 @@
-// import statement added by react_on_rails:generate_packs rake task
-import "./../generated/server-bundle-generated.js"
 import ReactOnRails from "react-on-rails";
 import PageWrapper from "../src/PageWrapper/ror_components/PageWrapper";
 
 ReactOnRails.register({
-  PageWrapper,
+  PageWrapper
 });
