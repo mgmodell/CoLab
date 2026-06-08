837db9ace399b435f6a1e1c1b0e46c9e8dbe9781
Micah Gideon Modell <micah.modell@gmail.com>
2026-06-08 17:41:58 +0000

    cleaned up the render wait

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,7 +18,11 @@ Warden.test_mode!
 
 def wait_for_render
   # 30 seconds should be more than enough for any page to render, even on CI
-  page.has_no_xpath?( "//*[@id='waiting']", wait: 30 )
+  retries = 10
+  while page.has_no_xpath?( "//*[@id='waiting']", wait: 1 ) and retries > 0
+    retries -= 1
+    # puts "Page failed to render within 10 seconds" if retries < 1
+  end
 end
 
 def ack_messages
