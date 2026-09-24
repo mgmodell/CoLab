b5f6dac705c8d856282a1f87054f8adaedc7092f
Micah Gideon Modell <micah.modell@gmail.com>
2026-09-24 01:58:47 +0000

    deprecating user.country

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 require 'faker'
 
 class User < ApplicationRecord
+  self.ignored_columnns += %w[country] # These are deprecated columns that we no longer use, but we need to keep them around for the time being to avoid breaking the schema
+
   include DeviseTokenAuth::Concerns::User
   include DeviseTokenAuth::Concerns::ResourceFinder
   include DeviseTokenAuth::Concerns::UserOmniauthCallbacks
